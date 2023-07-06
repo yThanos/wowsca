cd54653fb580b0a22143e47d78480bbc71b36152
Vitor Fraporti <vitorfraporti@hotmail.com>
2023-07-06 10:20:20 -0300

    x

--- src/main/java/jwt/validation/wowsca/db/ConectaDB.java
+++ src/main/java/jwt/validation/wowsca/db/ConectaDB.java
@@ -4,18 +4,14 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class ConectaDB {
-    private static final String DRIVER = "org.postgresql.Driver";
     private static final String URL = "jdbc:postgresql://localhost:5432/AppFinanceiro";
     private static final String USER = "postgres";
     private static final String SENHA = "1234";
 
     public Connection getConexao(){
         Connection con = null;
-        try {Class.forName(ConectaDB.DRIVER);
+        try {
             con = DriverManager.getConnection(ConectaDB.URL, ConectaDB.USER, ConectaDB.SENHA);
-        }
-        catch (ClassNotFoundException e){
-            e.printStackTrace();
         }
         catch (Exception e){
             e.printStackTrace();
